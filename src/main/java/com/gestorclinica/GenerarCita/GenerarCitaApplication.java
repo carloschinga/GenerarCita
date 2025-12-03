5d2386f5c8cfe6b6f9df27608acd8778fdfdf961
Carlos Enrique Chinga Ramos <chinga_carlos@hotmail.com>
2025-12-03 09:54:30 -0500

    ok

--- src/main/java/com/gestorclinica/GenerarCita/GenerarCitaApplication.java
+++ src/main/java/com/gestorclinica/GenerarCita/GenerarCitaApplication.java
@@ -2,12 +2,19 @@ package com.gestorclinica.GenerarCita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GenerarCitaApplication {
+public class GenerarCitaApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GenerarCitaApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GenerarCitaApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerarCitaApplication.class, args);
+    }
 
 }
